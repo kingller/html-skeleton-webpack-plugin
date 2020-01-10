664a0280b6f9718249d070243ca9d5c96bd7717c
Kingller Tang <kingller.tang@gaiaworks.cn>
2020-01-10 09:26:53 +0800

    优化

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ const error = msg => {
 class PandoraSkeletonWebpackPlugin {
   constructor(options = {}) {
     if (
-      typeof options.addToHtml !== 'undefined' && 
-      typeof options.addToHtml !== 'string' && 
-      typeof options.addToHtml !== 'object' && 
-      typeof options.addToHtml !== 'function'
+      typeof options.processHtml !== 'undefined' && 
+      typeof options.processHtml !== 'string' && 
+      typeof options.processHtml !== 'object' && 
+      typeof options.processHtml !== 'function'
     ) {
-      warn("options.addToHtml expected to be string or RegExp or function!");
+      warn("options.processHtml expected to be string or RegExp or function!");
     }
     if (typeof options.css !== 'undefined' && typeof options.css !== 'string') {
       warn("options.css expected to be string!");
     }
     this._options = {
-      addToHtml: options.addToHtml || /<!--\s*skeleton\s*-->/,
+      processHtml: options.processHtml || /<!--\s*skeleton\s*-->/,
       css: options.css,
       htmlPluginName: options.htmlPluginName || 'html-webpack-plugin',
     };
@@ -42,7 +42,7 @@ class PandoraSkeletonWebpackPlugin {
   }
   .pdr-page-content-skeleton {
     height: 100%;
-    background:url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjE4MCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cmVjdCBoZWlnaHQ9IjE2IiB3aWR0aD0iNDAlIiB5PSI5LjEwOSIgZmlsbC1vcGFjaXR5PSJudWxsIiBzdHJva2Utb3BhY2l0eT0ibnVsbCIgc3Ryb2tlLXdpZHRoPSIwIiBmaWxsPSIjZjJmMmYyIi8+PHJlY3QgaGVpZ2h0PSIxNiIgd2lkdGg9IjEwMCUiIHk9IjUwIiBmaWxsLW9wYWNpdHk9Im51bGwiIHN0cm9rZS1vcGFjaXR5PSJudWxsIiBzdHJva2Utd2lkdGg9IjAiIGZpbGw9IiNmMmYyZjIiLz48cmVjdCBoZWlnaHQ9IjE2IiB3aWR0aD0iMTAwJSIgeT0iODIiIGZpbGwtb3BhY2l0eT0ibnVsbCIgc3Ryb2tlLW9wYWNpdHk9Im51bGwiIHN0cm9rZS13aWR0aD0iMCIgZmlsbD0iI2YyZjJmMiIvPjxyZWN0IGhlaWdodD0iMTYiIHdpZHRoPSI3MCUiIHk9IjExNiIgZmlsbC1vcGFjaXR5PSJudWxsIiBzdHJva2Utb3BhY2l0eT0ibnVsbCIgc3Ryb2tlLXdpZHRoPSIwIiBmaWxsPSIjZjJmMmYyIi8+PC9zdmc+);
+    background:url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjE4MCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cmVjdCBoZWlnaHQ9IjE2IiB3aWR0aD0iNDAlIiB5PSI5IiBmaWxsPSIjZjJmMmYyIi8+PHJlY3QgaGVpZ2h0PSIxNiIgd2lkdGg9IjEwMCUiIHk9IjUwIiBmaWxsPSIjZjJmMmYyIi8+PHJlY3QgaGVpZ2h0PSIxNiIgd2lkdGg9IjEwMCUiIHk9IjgyIiBmaWxsPSIjZjJmMmYyIi8+PHJlY3QgaGVpZ2h0PSIxNiIgd2lkdGg9IjcwJSIgeT0iMTE2IiBmaWxsPSIjZjJmMmYyIi8+PC9zdmc+);
   }
   @keyframes pdr-page-skeleton-loading {
     0% {
@@ -56,28 +56,30 @@ class PandoraSkeletonWebpackPlugin {
   <div style="height: 100vh;">
     <div style="height: 55px;" class="pdr-page-head-skeleton"></div>
     <div style="height: calc(100vh - 55px);">
-      <div style="float: left; width: 300px; height: 100%; padding: 20px;">
+      <div style="float: left; width: 300px; height: 100%; padding: 20px 20px 0;">
          <div class="pdr-page-left-skeleton"></div>
       </div>
-      <div style="overflow: hidden; height: 100%; padding: 20px;">
+      <div style="overflow: hidden; height: 100%; padding: 20px 20px 0;">
         <div class="pdr-page-content-skeleton"></div>
       </div>
     </div>
   </div>
 `;
         let { html } = htmlPluginData;
-        let { css, addToHtml } = this._options;
+        let { css, processHtml } = this._options;
         if (typeof css !== 'string') {
           css = skeletonCss;
         }
-        html = html.replace(/<\/head>/, `<style type="text/css">
+        if (css) {
+          html = html.replace(/<\/head>/, `<style type="text/css">
   ${css}
 </style>
 </head>`);
-        if (typeof addToHtml === 'function') {
-          html = addToHtml(html);
+        }
+        if (typeof processHtml === 'function') {
+          html = processHtml(html, skeletonHtml);
         } else {
-          html = html.replace(addToHtml, skeletonHtml);
+          html = html.replace(processHtml, skeletonHtml);
         }
         htmlPluginData.html = html;
         if (callback) {
@@ -98,7 +100,7 @@ class PandoraSkeletonWebpackPlugin {
             const message = "Error running @pandora/skeleton-webpack-plugin, are you sure you have html-webpack-plugin before it in your webpack config's plugins?";
             error(message);
           }
-          hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync(PLUGIN_NAME, onBeforeHtmlGeneration);
+          hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync(PLUGIN_NAME, onBeforeHtmlProcessing);
         } else {
           const message = "Error running @pandora/skeleton-webpack-plugin, are you sure you have html-webpack-plugin before it in your webpack config's plugins?";
           error(message);
